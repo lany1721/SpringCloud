b5105f3cb88ef3de2f072d13aed8a10c0805ae7e
zpeace <1106124433@qq.com>
2020-12-25 10:19:13 +0800

    新增一个eureka应用，将eureka注册中心改为集群配置

---
+++ eureka-server7002/src/main/java/cn/zpeace/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package cn.zpeace.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zpeace
+ * @date 2020/12/25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
